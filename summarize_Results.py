aa378bc52b42c094588d47a01c5d67671a167e8b
theresaswayne <tcs6@columbia.edu>
2017-05-11 12:16:45 -0400

    Works in Fiji. Cleaned up and improved comments.

--- summarize_Results.py
+++ summarize_Results.py
@@ -4,16 +4,19 @@
 # Note: Do not change or remove the first two lines! They provide essential parameters.
 
 # summarize_Results.py
-# jython script  by Theresa Swayne, Columbia University, 2017 
-# usage: open in Fiji script editor and run (note jython is python 2.7 as of this writing) 
+# Jython script by Theresa Swayne, Columbia University, 2017 
+# usage: open in Fiji script editor, and run (note jython uses python 2.7 as of this writing) 
 
-# input: 3 csv files, C1_results, C2_results, Coloc (the beginning of the filename must be as given)
-# output: one csv file containing summarized data
-# if the output file exists then nothing is written to it
+# Input: 3 csv files in the input directory: 
+# C1_results, C2_results, Coloc (the beginning of the filename must be as given)
+# Output: one csv file containing summarized data
+# If the output file exists, then more data is appended to it
 
-# takes results written by the batch cfos-Arc analysis macro and summarize:
-# for each image: count particles, and calculate average and SD of (area, mean, intden, rawintden)
-# -------------  the input format: 
+# The input files should be written by the batch cfos-Arc analysis script.
+# For each image this script gives the cell count, average and SD of area, mean, intden, and rawintden
+# as well as the whole-image measurement for channel 1 and the colocalization data.
+
+# -------------  Required input columns: 
 # for channel results:
 # 0 rownumber, 1 label, 2 area, 3 mean, 4 min, 5 max, 6 x, 7 y, 8 intden, 9 rawintden
 # the label field has the filename; the first 3 chars are the channel (C1-), the last 9 chars are the ROI (:0000-0000)
@@ -22,7 +25,7 @@
 # for coloc results:
 # 0 Filename, 1 Channel, 2 Total Cells, 3 Colocalized Cells, 4 Fraction Colocalized
 
-# ----------------- output format
+# ----------------- Output columns:
 # 0 filename, 1-3 c1 whole image (mean, intden, rawintden), 
 # 4 c1 nuclei count, average and sd of c1 (5-6 area, 7-8 mean, 9-10 intden, 11-12 rawintden), 
 # 13 c2 nuclei count, average and sd of c2 (14-15 area, 16-17 mean, 18-19 intden, 20-21 rawintden)
@@ -33,21 +36,21 @@
 import os, csv, math, sys
 NUMCHANNELS = 2 # number of channels in the data
 
-# directory names for testing in Spyder
-input_ = "/Users/confocal/Desktop/input"
-output_ = "/Users/confocal/Desktop/ output"
+# for testing outside Fiji
+#input_ = "/Users/confocal/Desktop/input"
+#output_ = "/Users/confocal/Desktop/ output"
 
 # most methods require directory names to be expressed as strings
-#input_ = str(inputDir) # underscore to avoid using the name of a function
-#output_ = str(outputDir)
+input_ = str(inputDir) # underscore to avoid using the name of a function
+output_ = str(outputDir)
 
-# setup for output csv file in output directory
+# setup output file
 csvPath = output_ + os.sep + "Measurement_Summary.csv"
 csvExists = os.path.exists(csvPath)
 csvFile = open(csvPath, 'ab') # creates the file. a for append, b for binary (avoiding potential problems with ascii)
 csvWriter = csv.writer(csvFile) # this object is able to write to the output file
 
-# write headers of output file
+# add headers to output file
 if not csvExists: # avoids appending multiple headers
     headers1 = ['Filename','C1 Whole Image Mean','C1 Whole Image IntDen','C1 Whole Image RawIntDen']
     headers2 = ['C1 Nuc Count','C1 Ave Nuc Area','C1 StdDev Nuc Area','C1 Ave Nuc Mean','C1 StdDev Nuc Mean','C1 Ave Nuc IntDen','C1 StdDev Nuc IntDen','C1 Ave Nuc RawIntDen','C1 StdDev Nuc RawIntDen']
@@ -56,6 +59,8 @@ if not csvExists: # avoids appending multiple headers
     csvWriter.writerow(headers1+headers2+headers3+headers4)
 else:
     print("Appending to existing file.")
+
+# ---- helper functions for making file lists
 
 def uniqueElements(seq):
     '''
@@ -73,7 +78,6 @@ def _uniqueElements(seq):
     yields: a list of unique elements of seq, 
     preserving the order in which they were encountered
     '''
-    
     seen = set()
     for x in seq:
         if x in seen:
@@ -89,11 +93,10 @@ def MeanOfList(a):
     a: list of numbers
     returns a float, or "undefined" if list is empty
     '''
-    # avoid div by 0 error
-    if len(a) == 0:
+    if len(a) == 0: # avoid div by 0 error with empty list
         return "undefined"
 
-    result = 0.0 # have to use the decimal to make it a float
+    result = 0.0 # the decimal makes it a float
     total = 0.0
     for num in a:
         total += float(num)
@@ -109,8 +112,7 @@ def StdDevOfList(a):
     a: list of numbers
     returns: float
     '''
-    # avoid div by 0 error
-    if len(a) == 0:
+    if len(a) == 0: # avoid div by 0 error with empty list
         return "undefined"
 
     result = 0.0
@@ -125,28 +127,25 @@ def StdDevOfList(a):
     return result
 
 # ------------------ READING DATA
-
-# function to find files, create readers, and check the header format
-# this will be called twice
  
 def getInputFiles(input_dir,channel):
     '''
-    finds, checks, and reads input files
+    Finds, checks, and reads channel results files.
+    This is not necessary if the filenames are simple, 
+    but it could be helpful if implementing batch mode later.
     input_dir: the directory containing the files, a string
         ** the filenames must begin with "C1_results", "C2_results", etc.
     channel: the channel number, an integer
-    returns the filename for the indicated channel
-    this is not necessary if the filenames are simple, 
-    but it could be helpful if implementing batch mode later
+    returns: the filename for the indicated channel
+    If the format is incorrect, the script ends.
     '''
-
-    # the correct headers for the table
+    # required headers for the results file
     DESIREDHEADERS = ["", "Label", "Area", "Mean", "Min", "Max", "X", "Y", "IntDen", "RawIntDen"]
 
-    # the channel name we are looking for
+    # channel name we are looking for
     prefix = "C"+str(channel)+"_results"
     
-    # search the input directory and subdirectories
+    # search input directory and subdirectories
     for dirpath, dirnames, resultNames in os.walk(input_dir):
 
         # find channel file
@@ -154,12 +153,12 @@ def getInputFiles(input_dir,channel):
             if f.startswith(prefix):
                 CFilename = f
     
-    # open the file read-only
+    # open file
     CPath = os.path.join(input_,CFilename)
-    with open(CPath, 'rU') as CFile: # r for read-only, U = universal newline format, 'with' to auto-close file
+    with open(CPath, 'rU') as CFile: # r = read-only, U = universal newline format, 'with' to auto-close file
         CReader = csv.reader(CFile)
     
-        # check that the results are in the correct format
+        # gather headers from results file
         CHeaders = []
         i = 0
         for row in CReader:
@@ -167,7 +166,8 @@ def getInputFiles(input_dir,channel):
                 CHeaders = row
                 break # avoids reading entire file
             i += 1
-        
+            
+        # verify columns exist in required order
         for i in range(len(CHeaders)):
             try:
                 if CHeaders[i] != DESIREDHEADERS[i]:
@@ -182,47 +182,43 @@ def getInputFiles(input_dir,channel):
     return CFilename
 
 
-Filenames = [] # a list to store the channel result filenames and access them by index
+Filenames = [] # channel result filenames
 for i in range (1, NUMCHANNELS+1):
     Filenames.append(getInputFiles(input_,i))
 
-# open the C1 file and read the data into a list
+# Read C1 data
 C1Filename = Filenames[0]
 C1Path = os.path.join(input_,C1Filename)
 C1Data = []
 with open(C1Path, 'rU') as C1File: # r for read-only, U = universal newline format, 'with' to auto-close file
     C1Reader = csv.reader(C1File)
-    C1Reader.next() # skip the header row
+    C1Reader.next() # skip header row
     for row in C1Reader:
         C1Data.append(row)
         
-# open the C2 file and read the data into a list
+# Read C2 data
 C2Filename = Filenames[1]
 C2Path = os.path.join(input_,C2Filename)
 C2Data = []
 with open(C2Path, 'rU') as C2File: # r for read-only, U = universal newline format, 'with' to auto-close file
     C2Reader = csv.reader(C2File)
-    C2Reader.next() # skip the header row
+    C2Reader.next() # skip header row
     for row in C2Reader:
         C2Data.append(row)
 
-# open the Coloc file and read the data into a list
-# this has to be in the input directory
+# Read Coloc data
 ColocPath = os.path.join(input_,"Coloc.csv")
 ColocData = []
 with open(ColocPath, 'rU') as ColocFile: # r for read-only, U = universal newline format, 'with' to auto-close file
     ColocReader = csv.reader(ColocFile)
-    ColocReader.next() # skip the header row
+    ColocReader.next() # skip header row
     for row in ColocReader:
         ColocData.append(row)
 
-
-
 # ------------------- TRANSFERRING DATA 
 
-# collect image names
+# collect image names from both channel files
 Labels = []
-
 for row in C1Data:
     imageName = row[1] # 2nd column
     try:
@@ -234,7 +230,6 @@ for row in C1Data:
         imageName = imageName[3:]
     Labels.append(imageName)
     
-    
 for row in C2Data:
     imageName = row[1] # 2nd column
     try:
@@ -244,26 +239,24 @@ for row in C2Data:
         imageName = imageName[3:]
     Labels.append(imageName)
 
-# get a list of unique filenames
+# remove duplicates from the list
 imageList = uniqueElements(Labels)
 numRows = len(Labels)
 numFiles = len(imageList)
 
-
-# gather the data from one image -- rows with same image name
-for image in imageList:  # each file in the data batch
-
-    #Start a list to hold the summary data row. It should be filled with zeroes so we can fill in data for it of order
-    imageSummary = [0 for i in range(25)] # number of columns in desired output
-    imageSummary[0] = image
+# consolidate all the data from each image
+for image in imageList:  # each loop collects data from one image file
 
     print("processing", image)
 
-    # ---- collect data
+    # summary of all data for an image, filled with zeroes so we can fill in specific columns
+    imageSummary = [0 for i in range(25)] # number of columns in desired output
 
-    # Collect C1 data
+    # fill in the image name
+    imageSummary[0] = image
+
+    # collect C1 data
     
-    #Start lists to hold nuclei data per image -- mean, area, id, rid
     C1nucAreas = []
     C1nucMeans = []
     C1nucIDs = []
@@ -273,7 +266,7 @@ for image in imageList:  # each file in the data batch
         imageLabel = row[1]
         # check if filename is the same 
         if (imageLabel[3:-10] == image or imageLabel[3:] == image):
-            # now get the data
+            # get the data
             if ":" in imageLabel: # it's a nucleus
                 C1nucAreas.append(row[2])
                 C1nucMeans.append(row[3])
@@ -285,11 +278,10 @@ for image in imageList:  # each file in the data batch
                 imageSummary[3] = row[9]    
 
         # finished collecting data from this image
-        # cycle through all the rows then go to the next image name (inefficient but it works)
+        # will cycle through all rows then go to the next image name
     
     # collect C2 data
 
-    #Start lists to hold nuclei data per image -- mean, area, id, rid
     C2nucAreas = []
     C2nucMeans = []
     C2nucIDs = []
@@ -299,15 +291,14 @@ for image in imageList:  # each file in the data batch
         imageLabel = row[1]
         # check if filename is the same 
         if (imageLabel[3:-10] == image):
-            # now get the data
+            # get the data
             if ":" in imageLabel: # it's a nucleus
                 C2nucAreas.append(row[2])
                 C2nucMeans.append(row[3])
                 C2nucIDs.append(row[8])
                 C2nucRIDs.append(row[9])  
 
-
-    # collect coloc data
+    # collect Coloc data
     
     for row in ColocData:
         imageLabel = row[0]
@@ -317,11 +308,9 @@ for image in imageList:  # each file in the data batch
                 imageSummary[23] = row[4] # C1 with C2
             elif row [1] == "2":
                 imageSummary[24] = row[4] # C2 with C1
-
-
         
-    # ------------------CALCULATING STATISTICS
-    # 0 filename, 1-3 c1 whole image (mean, intden, rawintden), 
+    # ------------------ CALCULATING STATISTICS
+    # Output columns:
     # 4 c1 nuclei count, average and sd of c1 (5-6 area, 7-8 mean, 9-10 intden, 11-12 rawintden), 
     # 13 c2 nuclei count, average and sd of c2 (14-15 area, 16-17 mean, 18-19 intden, 20-21 rawintden)
 
@@ -345,12 +334,14 @@ for image in imageList:  # each file in the data batch
     imageSummary[20] = MeanOfList(C2nucRIDs)
     imageSummary[21] = StdDevOfList(C2nucRIDs)
 
+	# ----------------- WRITING DATA 
     csvWriter.writerow(imageSummary)
 
 # ----------------- FINISHING
 
 csvFile.close() # closes the output file so it can be used elsewhere
 
+print("Finished.")
 
 
 
