a20fd4d56cc98214c7518a4603dc7b8ece50faec
theresaswayne <tcs6@columbia.edu>
2018-02-06 15:51:08 -0500

    Logs and messages improved
    Completed IJ1 and IJ2 methods for showing status and progress in python

--- Demos and Tests/python_examples/logs_messages.py
+++ Demos and Tests/python_examples/logs_messages.py
@@ -5,7 +5,9 @@
 # Theresa Swayne, Feb. 2018
 # Demonstrates logs and messages that can be displayed in ImageJ using python 
 # Thanks to https://github.com/imagej/tutorials/blob/master/maven-projects/intro-to-imagej-api/src/main/java/IntroToImageJAPI.java
+# 
 # Watch the IJ toolbar when running this script!
+#
 
 import os, sys, time
 from java.lang import Double, Integer
@@ -17,27 +19,29 @@ import random
 startTime = time.clock()
 
 # imp = IJ.openImage("http://imagej.nih.gov/ij/images/blobs.gif");
-progress = int(round(100*random.random()))
+progress = round(100*random.random())
 
 # The ImageJ1 methods for showing messages include log, status, and error
 
 # Prints to the ImageJ Log window (note this is an ImageJ1 function, and IJ2 functions are preferred) 
 IJ.log("I'm an IJ.log message!")
 
+# The error function produces a dialog with "OK" button 
+IJ.error("I'm an IJ1 error! Watch the toolbar!")
+
 # This shows a progress bar in the IJ toolbar.
-IJ.showProgress(progress)
+IJ.showProgress(progress/200.0)
 
 # The IJ1 status message shows up in the status bar (toolbar) until it is replaced by another message
 IJ.showStatus("Here I am in the toolbar")
 time.sleep(3) # pause so you can see the message
 
-# The error message shows up in a dialog box
-IJ.error("I'm an IJ1 error!")
+# The bar is erased if currentValue>=1.0
+IJ.showProgress(1.0)
 
 # Print statements show up in the script editor if it is open
 # Don't know where they show up if the editor is not open...
 print "I am appearing unobtrusively at the bottom of the Script Editor."
-print "progress = " + str(progress) + "%"
 
 # The preferred IJ2 alternatives to the Log window are the Log Service and Status Service.  
 # Log Service messages appear in the Console window and are more designed for programmers.
@@ -58,16 +62,18 @@ logService.trace("What does trace even do?")
 
 # This is the IJ2 version of showing status, using the StatusService loaded in the script parameters
 ss.showStatus(int(progress), 100, "Processing " + str(progress) + "%")
-time.sleep(3)
+time.sleep(2) # pause so you can see the message
+ss.showStatus(100, 100, "Processing complete")
+time.sleep(2)
 
 ss.showStatus("This status message will be replaced by the IJ version after I clear it.")
 time.sleep(3)
 
 ss.clearStatus()
-time.sleep(3)
+time.sleep(1)
 
-# This produces a dialog with "OK" button 
-ss.warn("I'm warning you!")
+# The Status Service warning produces a dialog with "OK" button 
+ss.warn("I'm an IJ2 warning. Bye!")
 
 endTime = time.clock()
 elapsedTime = endTime - startTime
