fcb60c96340d5f466c4c65d6a6d7d010e21da70a
Theresa Swayne <theresa@Mini-Swemtage.local>
2018-04-08 10:07:01 -0400

    actin: calculated perpendiculars, adding back to the main script

--- actin_analysis/constructPerp.py
+++ actin_analysis/constructPerp.py
@@ -1,8 +1,6 @@
 # constructPerp.py
 # given points a and b, 
-# draws a line between them 
 # and finds the endpoints c and d of a perpendicular line of length r, through point b 
-# and draws that line
 
 import random
 import math
@@ -10,36 +8,85 @@ import math
 # establish the points a and b and the length r
 
 # fixed numbers
-# xa = 20.0
-# xb = 30.0
-# ya = 40.0
-# yb = 50.0
+xa = 20.0
+ya = 45.0
+xb = 60.0
+yb = 75.0
 
 random.seed(9)
 
-xa = float(random.randrange(10,100))
-ya = float(random.randrange(10,100))
+#xa = float(random.randrange(10,100))
+#ya = float(random.randrange(10,100))
 
-xb = float(random.randrange(10,100))
-yb = float(random.randrange(10,100))
+#xb = float(random.randrange(10,100))
+#yb = float(random.randrange(10,100))
 
-r = 20.0
+profileLength = 20.0
 
-# check for boundary conditions -- horizontal or vertical line
+print(xa, ya, xb, yb)
 
-if (xa-xb == 0): # vertical line, slope and intercept are undefined
+# get equations for the line and perpendicular, checking for boundary conditions
+# Line ab: y = m * x + b
+# Perpendicular line: y = mp * x + bp
+# calculate endpoints of new perpendicular line of length r: (xc,yc) and (xd,yd)
+ 
+if (xa-xb == 0.0): # vertical line, slope and intercept are undefined
 
-	# slope of perp mp = 0
-	# intercept of perp bp = yb 
-	print("the line is vertical")
+	print("the original line is vertical")
+	# slope m is undefined, slope of perp mp = 0
+	mp = 0.0
+	# intercept b is undefined, intercept of perp bp = yb
+	bp = yb
+
+	# find points c and d on the horizontal line
+	xc = xb + profileLength/2.0
+	yc = yb
+
+	xd = xb - profileLength/2.0
+	yd = yb
 	
-else if (ya - yb == 0): # horizontal line, slope and intercept of perp are undefined
-
-	# get points c and d by simply adding 
-	print("the line is horizontal")
 	
-else: # calculate slope and intercept of line ab in the normal way
+elif (ya - yb == 0.0): # horizontal line, slope and intercept of perp are undefined
 
-	print("the line is diagonal")
+	print("the original line is horizontal")
+	# slope m is 0, slope of perp mp = undefined
+	m = 0.0
+	# intercept b is yb, intercept of perp bp is undefined
+	b = yb
+
+	# find points c and d on the vertical line
+	xc = xb
+	yc = yb + profileLength/2.0
+
+	xd = xb
+	yd = yb - profileLength/2.0
+
+	
+else: # calculate slopes and intercepts in the normal way
+
+	# slope m is change in y over change in x
 	m = (yb-ya)/(xa-xb)
+	# intercept b is obtained by solving the equation
+	b = yb - (m*xb)
+	print("original line slope = "+str(m)+", intercept = "+str(b))
 
+	mp = -1/m
+	bp = xb * ((m**2+1.0)/m) + b # must use ## not ^!
+	print("perpendicular line slope = "+str(mp)+", intercept = "+str(bp))
+
+	# find points c and d on the perpendicular unit vector through point b
+	# thanks to David Nehme on Stack Overflow
+
+	abDist = math.sqrt((xb-xa)**2 + (yb-ya)**2)
+
+	dx = (xb-xa)/abDist
+	dy = (yb-ya)/abDist
+
+	xc = xb + (profileLength/2)*dy
+	yc = yb - (profileLength/2)*dx
+
+	xd = xb - (profileLength/2)*dy
+	yd = yb + (profileLength/2)*dx
+
+print(xa, ya, xb, yb)
+print(xc, yc, xd, yd)
--- actin_analysis/drawPerpendiculars.py
+++ actin_analysis/drawPerpendiculars.py
@@ -10,11 +10,23 @@ from ij.gui import Roi, PolygonRoi, FreehandRoi
 from ij.plugin.frame import RoiManager
 import random
 
+# ---- parameters
+
+profileSpacing = 10.0 # how far apart profiles should be along the cable (in scaled units)
+slopeCalcSpacing = 2 # int, how far away we look along the line to calculate the slope (in pixels)
+profileLength = 20.0 # total length of profile line (in pixels for now)
+profileWidth = 1.0 # width of profile line (in pixels for now)
 
 # ---- create image for testing
 
 imp = IJ.createImage("test", "16-bit black", 200, 200, 1)
 imp.show()
+
+# ---- TODO: calculate scaled parameters
+
+imageCalib = imp.getCalibration()
+pixSize = imageCalib.pixelHeight # assuming in microns
+profileSpacingPix = profileSpacing/pixSize # float
 
 # ---- create a test ROI from a set of coordinates
 
@@ -60,13 +72,113 @@ rm.select(0)
 # ---- sample the line evenly
 
 sampInt = 2 # pixel units
-sampLine = testLine.getInterpolatedPolygon(sampInt,False)
+sampLine = testLine.getInterpolatedPolygon(sampInt,False) # a FloatPolygon with evenly spaced points
 
 sampLength = sampLine.getLength(True) # True means it's a line
 
-sampX = sampLine.xpoints
-sampY = sampLine.ypoints
+sampX = sampLine.xpoints # float array
+sampY = sampLine.ypoints # float array
 
 # note the supposed length of the straight line comes out between 113 and 99 depending on the sampInt value, 1 to 100
 # note for a crooked line, the number of samples is consistently half the length for sampInt = 2
 print("The line is " + str(sampLength) + " pixels long and there are " + str(len(sampX)) + " samples.")
+
+
+# ---- helper function for finding perpendicular at a point
+
+def findPerp(xa, ya, xb, yb, perpLength):
+	"""returns a tuple containing the endpoints of a line segment
+	perpendicular to the line defined by (xa,ya) and (xb,yb),
+	passing through (xb,yb), of length perpLength
+	all parameters = floats
+	"""
+	# get equations for the line and perpendicular, checking for boundary conditions
+	# Line ab: y = m * x + b
+	# Perpendicular line: y = mp * x + bp
+	# calculate endpoints of new perpendicular line of length r: (xc,yc) and (xd,yd)
+	 
+	if (xa-xb == 0.0): # vertical line, slope and intercept are undefined
+	
+		print("the original line is vertical")
+		# slope m is undefined, slope of perp mp = 0
+		mp = 0.0
+		# intercept b is undefined, intercept of perp bp = yb
+		bp = yb
+	
+		# find points c and d on the horizontal line
+		xc = xb + profileLength/2.0
+		yc = yb
+	
+		xd = xb - profileLength/2.0
+		yd = yb
+		
+		
+	elif (ya - yb == 0.0): # horizontal line, slope and intercept of perp are undefined
+	
+		print("the original line is horizontal")
+		# slope m is 0, slope of perp mp = undefined
+		m = 0.0
+		# intercept b is yb, intercept of perp bp is undefined
+		b = yb
+	
+		# find points c and d on the vertical line
+		xc = xb
+		yc = yb + profileLength/2.0
+	
+		xd = xb
+		yd = yb - profileLength/2.0
+	
+		
+	else: # calculate slopes and intercepts in the normal way
+	
+		# slope m is change in y over change in x
+		m = (yb-ya)/(xa-xb)
+		# intercept b is obtained by solving the equation
+		b = yb - (m*xb)
+		print("original line slope = "+str(m)+", intercept = "+str(b))
+	
+		mp = -1/m
+		bp = xb * ((m**2+1.0)/m) + b # must use ## not ^!
+		print("perpendicular line slope = "+str(mp)+", intercept = "+str(bp))
+	
+		# find points c and d on the perpendicular unit vector through point b
+		# thanks to David Nehme on Stack Overflow
+	
+		abDist = math.sqrt((xb-xa)**2 + (yb-ya)**2)
+	
+		dx = (xb-xa)/abDist
+		dy = (yb-ya)/abDist
+	
+		xc = xb + (profileLength/2)*dy
+		yc = yb - (profileLength/2)*dx
+	
+		xd = xb - (profileLength/2)*dy
+		yd = yb + (profileLength/2)*dx
+
+	return xc, yc, xd, yd # a tuple
+
+# --- step along the line
+# at intervals defined by profileSpacing, calculate the perpendicular line using slopeCalcSpacing
+
+profSpacingPix = int(profileSpacingPix)
+
+for i in range(1, len(sampX)-1, profSpacingPix): 
+
+	# TODO: slope calc should be ints
+	# define the points used to calculate the slope
+	xa = sampX[i-slopeCalcSpacing]
+	ya = sampX[i-slopeCalcSpacing]
+	xb = sampX[i]
+	yb = sampY[i]
+
+	# calculate the endpoints of the perp
+	profCoords = findPerp(xa, ya, xb, yb, perpLength)
+	xc = profCoords[0]
+	yc = profCoords[1]
+	xd = profCoords[2]
+	yd = profCoords[3]
+
+	# make the perp line ROI
+	perpLine = Line(xc, yc, xd, yd)
+	perpLine.setWidth(profileWidth)
+	rm.addRoi(perpLine)
