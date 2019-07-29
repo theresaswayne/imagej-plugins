bdf283499fb1aa4a289d5bcbc4cb04340c663016
theresaswayne <tcs6@columbia.edu>
2019-07-29 16:20:59 -0400

    Phagocytosis -- updated summary table reformatting

--- phagocytosis_particles/extract_text_spread.R
+++ phagocytosis_particles/extract_text_spread.R
@@ -6,17 +6,17 @@
 require(stringr)
 require(tidyverse)
 
+# load file
+mydatafile <- file.choose()
+mydata <- read.csv(mydatafile) # we have the full file path
+
 # sample names containing info, e.g. labels from ImageJ analysis
 
-label <- c("C1-synuclein phago test2_syn mono 1 - Aligned-MaxIP-1-t1",
-             "synuclein phago test2_syn mono 1 - Aligned-MaxIP-1-t1-Particles inside",
-             "synuclein phago test2_syn mono 1 - Aligned-MaxIP-1-t1-Total particles",
-             "C1-synuclein phago test2_syn mono 1 - Aligned-MaxIP-1-t2",
-             "synuclein phago test2_syn mono 1 - Aligned-MaxIP-1-t2-Particles inside",
-             "synuclein phago test2_syn mono 1 - Aligned-MaxIP-1-t2-Total particles")
+# labels are first column
+label <- mydata[,1]
 
-# sample data that we ultimately want to associate with times
-count <- c(85,71,102,85,75,115)
+# counts are second column
+count <- mydata[,2]
 
 # str_match returns the matching text in column 1 and the capture group (in parens) in column 2
 # we only want column 2 here.
@@ -26,10 +26,7 @@ time <- str_match(label, "-t([0-9]*)")[,2]
 # extract the info telling us which type of measurement it is
 type_in <- str_match(label, "Particles inside")[,1]
 type_total <- str_match(label, "Total particles")[,1]
-type_cells <- str_match(label, "[0-9]$")[,1]
-
-# transform all non-NA values in type_cells to "Total cells"
-type_cells <- replace(type_cells,!is.na(type_cells), "Total cells")
+type_cells <- str_match(label, "Nuclei")[,1]
 
 # merge the measurement types into a single column (inelegantly)
 types <- cbind(type_in, type_total, type_cells)
@@ -48,6 +45,7 @@ df <- data.frame(label, time, merged_types, count)
 df$label <- gsub("-t[0-9]*", "", df$label)
 df$label <- gsub("-Particles inside", "", df$label)
 df$label <- gsub("-Total particles", "", df$label)
+df$label <- gsub("-Nuclei", "", df$label)
 df$label <- gsub("C1-", "", df$label)
 
 # Reshape the table so that each timepoint is a row and the types become variables
