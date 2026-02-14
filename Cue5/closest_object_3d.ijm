e5228f6a0d949bee94d5ebeb10876e501b153fe1
Theresa Swayne <theresaswayne@users.noreply.github.com>
2026-02-14 18:26:57 -0500

    Created single macro to find and measure colocalized objects

---
+++ Cue5/closest_object_3d.ijm
@@ -0,0 +1,165 @@
+//@ File(label = "Nup label image:") NupFile
+//@ File(label = "Erg label image:") ErgFile
+//@ File(label = "Output folder:", style = "directory") outDir
+//@ Double(label = "Distance criterion (µm):", value = 0.9) dist
+// closest_object_3d.ijm
+// Visualize 3D closest objects
+// input: 2 label image stacks in a specific order; distance criterion
+// output: multichannel stack with channel 1= all objects in stack 1 that are within the given distance (center-center) of an object in stack 2
+// channel 2 = all objects in stack 2 that are within that distance of some object in stack 1 
+
+// setup general
+while (nImages>0) { // clean up open images
+	selectImage(nImages);
+	close();
+	}
+print("\\Clear"); // clear Log window
+
+
+// open datasets
+open(NupFile);
+nupTitle = getTitle();
+// determine the name of the file without extension
+dotIndex = lastIndexOf(nupTitle, ".");
+nupBasename = substring(nupTitle, 0, dotIndex);
+
+
+open(ErgFile);
+ergTitle = getTitle();
+dotIndex = lastIndexOf(ergTitle, ".");
+ergBasename = substring(ergTitle, 0, dotIndex);
+
+// find objects matching criteria
+run("3D Distances Closest", "image_a="+nupBasename+" image_b="+ergBasename+" number=1 distance=DistCenterCenterUnit distance_maximum="+dist);
+
+// save the data
+distTableName = "ClosestObjectsWithinCriterion.csv";
+saveAs("Results", outDir + File.separator + distTableName);
+
+// read the results
+
+rowCount = getValue("results.count");
+nupColocs = newArray();
+nupNonColocs = newArray();
+ergColocs = newArray();
+
+colocCount = 0;
+nonColocCount = 0;
+
+if (rowCount > 0) { // if there are any colocalization (behavior varies; may be one row per object or not)
+
+	for (i = 0; i < rowCount; i++) { // go through the table
+	
+		nupNum = Table.get("LabelObj", i); // each nup object will have a row whether or not it meets criteria
+		ergNum = Table.get("O1", i); // will be 0 if no match
+		
+		// check if there is a matching Erg object and if so, add to the array of colocs
+		if (ergNum > 0) {
+			
+			nupColocs[colocCount] = nupNum;
+			ergColocs[colocCount] = ergNum;
+			colocCount = colocCount + 1;
+		}
+		else {
+			nupNonColocs[nonColocCount] = nupNum;
+			nonColocCount = nonColocCount + 1;
+		}
+	}
+	
+
+
+	print("Colocalizing Nups:");
+	Array.print(nupColocs);
+	print("Non-colocalizing Nups:");
+	Array.print(nupNonColocs);
+	print("Colocalizing Ergs:");
+	Array.print(ergColocs);
+}
+else {
+	print("No data in table between",nupTitle, "and",ergTitle);
+}
+print("total colocalizations: ",colocCount);
+
+
+// initialize 3D functions
+run("3D Manager");
+Ext.Manager3D_Reset();
+// options: important to NOT show as IJ results table beause it conflicts with the other table
+run("3D Manager Options", "volume feret distance_to_surface objects radial_distance distance_between_centers distance_max_contact drawing=Contour use_0");
+
+// generate an image of only the colocalizing Nups
+selectWindow(nupTitle);
+run("Duplicate...", "title=nupColoc duplicate");
+Ext.Manager3D_AddImage();
+Ext.Manager3D_DeselectAll();
+
+Ext.Manager3D_MultiSelect();
+// object numbers start at 1, ROI indices start at 0
+for (j = 0; j < nonColocCount; j++) {
+	nupObject = nupNonColocs[j];
+	nupIndex = nupObject-1;
+	print("Selecting ROI index",nupIndex,",Nup object number",nupObject);
+	Ext.Manager3D_Select(nupIndex);
+}
+
+Ext.Manager3D_Erase(); // fill with black in the duplicated stack
+Ext.Manager3D_DeselectAll();
+Ext.Manager3D_Measure(); // measure only the coloc nups
+
+// save the image
+selectWindow("nupColoc");
+saveAs("Tiff", outDir + File.separator + "nupColoc.tif");
+
+// save the measurements
+Ext.Manager3D_SaveResult("M",outDir + File.separator + "nupColocMeas.csv");
+Ext.Manager3D_CloseResult("M");
+
+
+// --- Erg coloc ----
+
+// initialize 3D functions
+Ext.Manager3D_Reset();
+run("3D Manager");
+Ext.Manager3D_Reset();
+// options: important to NOT show as IJ results table beause it conflicts with the other table
+run("3D Manager Options", "volume feret distance_to_surface objects radial_distance distance_between_centers distance_max_contact drawing=Contour use_0");
+
+// generate an image of only the colocalizing Ergs
+selectWindow(ergTitle);
+run("Duplicate...", "title=ergColoc duplicate");
+Ext.Manager3D_AddImage();
+Ext.Manager3D_DeselectAll();
+
+// make a list of noncolocalizing Ergs, that is everything that is not in the ergColocs array
+Ext.Manager3D_Count(ergCount);
+ergNonColocs = Array.getSequence(ergCount+1);
+ergNonColocs = Array.deleteValue(ergNonColocs, 0);// start with a list of all erg obj numbs starting with 1
+for (idx = 0; idx < colocCount; idx++) {
+	ergObj = ergColocs[idx];
+	ergNonColocs = Array.deleteValue(ergNonColocs, ergObj); // delete the object number from the array and make the array shorter
+}
+ergNonColocCount = lengthOf(ergNonColocs);
+
+Ext.Manager3D_MultiSelect();
+// object numbers start at 1, ROI indices start at 0
+
+for (k = 0; k < ergNonColocCount; k++) { // loop over the non-coloc ergs in the roi mgr
+
+	ergObject = ergNonColocs[k];
+	ergIndex = ergObject-1;
+	print("Selecting ROI index",ergIndex,",Erg object number",ergObject);
+	Ext.Manager3D_Select(ergIndex);
+}
+
+Ext.Manager3D_Erase(); // fill with black in the duplicated stack
+Ext.Manager3D_DeselectAll();
+Ext.Manager3D_Measure(); // measure only the coloc ergs
+
+// save the image
+selectWindow("ergColoc");
+saveAs("Tiff", outDir + File.separator + "ergColoc.tif");
+
+// save the measurements
+Ext.Manager3D_SaveResult("M",outDir + File.separator + "ergColocMeas.csv");
+Ext.Manager3D_CloseResult("M");
+
---
+++ Cue5/survival_analysis_multiple_fields.R
@@ -0,0 +1,212 @@
+# R script to generate survival curves from cell interaction data
+# Assumptions:
+# Each field of view is represented by one CSV file 
+# Required cols: filename, Label, timepoint (others are ignored)
+# filename data contains experiment and ROI name in the pattern Wellxx ROIy FrameZZZ.csv
+# Label data is of the format objA1_objB7 
+# Outputs:
+# number of objects in the A class that show interactions
+# number of objects in the A class that show persistent interactions 
+
+# To use: 
+# 1) Generate merged colocalization files, one per imaging field, using combine_csv_files_with_num.R
+#     Place all files in the same folder          
+# 2) Edit Parameters section (object names, time window, interaction threshold) as needed
+# 3) Source the script; when prompted, open the folder containing the files
+
+# ---- Setup ----
+
+require(tidyverse) # for data processing
+require(stringr) # for string harvesting
+require(RcppRoll) # for scanning interactions
+require(xfun) # for filename manipulation
+require(survival) # for survival analysis
+require(survminer) # for survival analysis
+require(ggplot2) # for survival output
+require(broom) # for survival output
+ 
+# ---- Parameters ----
+# change these as needed 
+
+# object A represents a Treg and object B represents a fibroblast
+objAname = "Treg"
+objBname = "Fibroblast"
+
+# time window size (units = timepoints) for checking persistence of interactions
+# threshold is the number of interactions within that window to be considered consistent
+# if time window == threshold, then you must have that many consecutive interactions
+# if time window > threshold, then the interactions may be non-consecutive but must occur within the window
+
+timeWindow <- 22
+threshold <- 8
+
+# ---- Get the data ----
+# no message will be displayed. Choose the file to analyze
+selectedFile <- file.choose()
+parentFolder <- dirname(selectedFile) # parent of the selected file
+df <- read_csv(selectedFile)
+
+# ---- Reformat the data ----
+
+# remove unneeded columns
+df <- df %>% 
+  select(filename, Label, timepoint) # all rows
+
+# split Label to identify object names
+# format: objA1_objB7
+df <- df %>%
+  separate_wider_delim("Label", delim="_", names=c("ObjA", "ObjB"))
+
+# get the Object A numbers by capturing what's after "objA"
+Anums <- str_replace(df$ObjA, "objA(.*)", "\\1")
+
+# get the Object B numbers by capturing what's after "objB"
+Bnums <- str_replace(df$ObjB, "objB(.*)", "\\1")
+
+# substitute the cleaned data for the original
+df <- df %>% mutate(ObjA = Anums,
+                    ObjB = Bnums)
+
+# ---- Count interactions ----
+
+# total interactions per timepoint
+intPerTime <- df %>% 
+  count(timepoint)
+
+# total interactions per T cell (objA)
+intPerA <- df %>%
+  count(ObjA) %>% 
+  arrange(as.integer(ObjA))
+
+# ---- Generate table of interactions across time ----
+
+timepoints <- min(df$timepoint):max(df$timepoint) # unique row for each timepoint
+
+objA_IDs <- as.character(sort(as.integer(unique(df$ObjA)))) # all the interacting objects across the dataset
+
+# create a logical vector for each objA containing the interacting timepoints for a single object
+# and assemble these into a table of interactions over time
+
+intxns <- data.frame("t" = timepoints) # this serves as the 1st col
+
+for (obj in objA_IDs) {
+  # objInts and objTimes = temporary variables
+  objInts <- df %>% 
+    filter(ObjA == obj) # select rows pertaining to one objA
+  objTimes <- timepoints %in% objInts$timepoint # boolean: at which timepoints was that object interacting?
+  intxns[[paste0("",obj)]] <- objTimes # add a column showing interaction status for that object
+}
+
+# check the total calculation (compare to intPerA above)
+intPerALogical <- intxns[,-1]  #omit the t column
+intPerALogical <- intPerALogical %>% 
+  pivot_longer(cols = everything(), names_to = "ObjA", values_to = "val") %>%
+  group_by(ObjA) %>%
+  summarise(Total = sum(val)) %>% 
+  arrange(as.integer(ObjA))
+
+# calculate a rolling sum along the logical vector
+
+# function to return TRUE if any rolling sums 
+#  within the time window meet or exceed the threshold 
+#  (may be consecutive or non-consecutive contacts)
+
+intxnsWithoutT <- intxns[,-1] # omit the t column
+persist <- intxnsWithoutT %>%
+  summarise_all(function(x) {
+    ifelse(max(roll_sum(x, n = timeWindow, weights = NULL, fill = numeric(0),
+                        partial = FALSE, align = "left", normalize = TRUE,
+                        na.rm = FALSE)) >= threshold, TRUE, FALSE)
+  })
+
+# ---- Calculate % persistence ----
+
+# pivot the table to get the number and % persistent
+persistSummary <- persist %>% 
+  pivot_longer(cols = everything(), names_to = "ObjA", values_to = "Persistent")
+
+totalIntxns <- nrow(persistSummary)
+totalPersistent <- sum(persistSummary$Persistent == TRUE)
+fracPersistent <- totalPersistent/totalIntxns
+
+# ---- Survival analysis ----
+
+# start with the table of interactions
+# filter for those objects undergoing persistent interactions
+# for each object, we will analyze the first interaction only
+# -- assuming that each Treg is unlikely to interact with multiple Fbs
+# -- this in turn assumes tracking is accurate (same Fb is always assigned the same object number)
+
+# create an empty table to hold the results
+survData <- data.frame(ObjA = character(),
+                       Duration = double(),
+                       Status = double(),
+                       stringsAsFactors = FALSE)
+
+# which objAs have persistent interactions?
+persistObjs <- persistSummary$ObjA[persistSummary$Persistent == TRUE]
+persistObjs <- as.character(sort(as.integer(unique(persistObjs)))) 
+
+for (obj in persistObjs) {
+  # select the object column and find the start time (earliest timepoint of interaction)
+  intxnTimes <- intxns %>% 
+    filter(intxns[[obj]] == TRUE)
+  intxnStart <- min(intxnTimes$t)
+  
+  # find the end time of that interaction and the total duration
+  laterTimes <- intxns %>% filter(t > intxnStart)
+  intxnEnd <- min(laterTimes$t[laterTimes[[obj]] == FALSE])
+  
+  # handle interactions at end of expt
+  # set status 1 = event (end of interaction), 0 = censored (still "alive" at end of experiment)
+  if(is.infinite(intxnEnd)) {
+    status <- 0
+    }
+  else {
+    status <- 1
+    }
+  intxnDur <- intxnEnd - intxnStart
+  # create a new row in the data table 
+  survData <- survData %>% add_row(ObjA = obj,
+          Duration = intxnDur, 
+          Status = status)
+  }
+
+# fit and plot the survival curve using Kaplan-Meier method
+fit1 <- survfit(Surv(Duration, Status) ~ 1, data=survData)
+p <- ggsurvplot(fit1, data = survData, risk.table = FALSE,
+                submain = "Kaplan-Meier survival curve",
+)
+# ---- Create output ----
+
+resultHeaders <- c("Filename", "Window", "Threshold", "Total Interacting",  "Persistent Interacting","Fraction Persistent")
+resultValues <- c(basename(selectedFile), timeWindow, threshold, totalIntxns, totalPersistent, fracPersistent)
+resultTable <- data.frame(rbind(resultHeaders, resultValues))
+names(resultTable) <- resultTable[1,]
+resultTable <- resultTable[-1,]
+
+# intxns 
+intxnFile = paste(sans_ext(basename(selectedFile)), "_interactions_",timeWindow, "_",threshold,".csv", sep = "")
+write_csv(intxns,file.path(parentFolder, intxnFile))
+
+# survival table
+survTable = paste(sans_ext(basename(selectedFile)), "_surv_",timeWindow, "_",threshold,".csv", sep = "")
+write_csv(survData,file.path(parentFolder, survTable))
+
+# survival fit table
+survSumm <- tidy(fit1)
+survFit = paste(sans_ext(basename(selectedFile)), "_survFit_",timeWindow, "_",threshold,".csv", sep = "")
+write_csv(survSumm,file.path(parentFolder, survFit))
+
+# survival plot
+plotFile = paste(sans_ext(basename(selectedFile)), "_plot_",timeWindow, "_",threshold,".png", sep = "")
+ggsave(filename = plotFile, path = parentFolder)
+
+# persistSummary (which objects showed persistent interactions)
+persistFile = paste(sans_ext(basename(selectedFile)), "_persistence_",timeWindow, "_",threshold,".csv", sep = "")
+write_csv(persistSummary,file.path(parentFolder, persistFile))
+
+# summary (final calculations)
+resultFile = paste(sans_ext(basename(selectedFile)), "_summary_",timeWindow, "_",threshold,".csv", sep = "")
+write_csv(resultTable,file.path(parentFolder, resultFile))
+
