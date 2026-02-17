cbc3aa113180d1f433b05e914273c0932fc6c931
Theresa Swayne <theresaswayne@users.noreply.github.com>
2026-02-17 16:11:00 -0500

    update batch shuffle

--- Cue5/shuffle_labels_batch.R
+++ Cue5/shuffle_labels_batch.R
@@ -74,79 +74,88 @@ process_file_func <- function(f, out) {
   nups <- dataWithGroups %>% filter(Group == "Nup") %>% select(c("CX (unit)", "CY (unit)", "CZ (unit)"))
   ergs <- dataWithGroups %>% filter(Group == "Erg") %>% select(c("CX (unit)", "CY (unit)", "CZ (unit)"))
   
-  ##nn2 nearest neighbor
-  #ergNearNup <- nn2(nups, query=ergs, k=1, searchtype = "radius", radius = distCriterion) # produces an output with n(erg) evaluations
-  nupNearErg <- nn2(ergs, query=nups, k=1, searchtype = "radius", radius = distCriterion) # this is what we want, n(nup) evaluations
+  ##nn2 nearest neighbor -- do only if there are objects in both sets
   
-  # If there are no neighbours then nn.idx will contain 0 and nn.dists will contain 1.340781e+154 for that point.
-  totalColoc <- sum(nupNearErg[[1]] != 0) # looks at the nn.idx showing the index of the rows that are closest
-  
-  # Monte Carlo simulation
-  
-  # vector to store results
-  results = numeric(trials)
-  
-  for (i in 1:trials) {
-    
-    # resample randomly without replacement
-    shuffledGroups <-sample(origGroups)
-    
-    # check count
-    #nupShuf <- sum(shuffledGroups == "Nup")
-    
-    # generate datasets
-    shufNups <- dataWithGroups %>% filter(shuffledGroups == "Nup") %>% select(c("CX (unit)", "CY (unit)", "CZ (unit)"))
-    shufErgs <- dataWithGroups %>% filter(shuffledGroups == "Erg") %>% select(c("CX (unit)", "CY (unit)", "CZ (unit)"))
-    
-    ##nn2 nearest neighbor
-    shufNupNearErg <- nn2(shufErgs, query=shufNups, k=1, searchtype = "radius", radius = distCriterion) # this is what we want, n(nup) evaluations
+  if (nupCount != 0 & ergCount != 0) {
+    #ergNearNup <- nn2(nups, query=ergs, k=1, searchtype = "radius", radius = distCriterion) # produces an output with n(erg) evaluations
+    nupNearErg <- nn2(ergs, query=nups, k=1, searchtype = "radius", radius = distCriterion) # this is what we want, n(nup) evaluations
     
     # If there are no neighbours then nn.idx will contain 0 and nn.dists will contain 1.340781e+154 for that point.
-    shufColoc <- sum(shufNupNearErg[[1]] != 0) # looks at the nn.idx showing the index of the rows that are closest
+    totalColoc <- sum(nupNearErg[[1]] != 0) # looks at the nn.idx showing the index of the rows that are closest
     
-    # append to results
-    results[i] <- shufColoc
+    # Monte Carlo simulation
+    
+    # vector to store results
+    results = numeric(trials)
+    
+    for (i in 1:trials) {
+      
+      # resample randomly without replacement
+      shuffledGroups <-sample(origGroups)
+      
+      # check count
+      #nupShuf <- sum(shuffledGroups == "Nup")
+      
+      # generate datasets
+      shufNups <- dataWithGroups %>% filter(shuffledGroups == "Nup") %>% select(c("CX (unit)", "CY (unit)", "CZ (unit)"))
+      shufErgs <- dataWithGroups %>% filter(shuffledGroups == "Erg") %>% select(c("CX (unit)", "CY (unit)", "CZ (unit)"))
+      
+      ##nn2 nearest neighbor
+      shufNupNearErg <- nn2(shufErgs, query=shufNups, k=1, searchtype = "radius", radius = distCriterion) # this is what we want, n(nup) evaluations
+      
+      # If there are no neighbours then nn.idx will contain 0 and nn.dists will contain 1.340781e+154 for that point.
+      shufColoc <- sum(shufNupNearErg[[1]] != 0) # looks at the nn.idx showing the index of the rows that are closest
+      
+      # append to results
+      results[i] <- shufColoc
+    }
+    
+    # rank the actual results vs the shuffled
+    results_plus_expt <- c(results, totalColoc)
+    expt_rank_pct <- percent_rank(results_plus_expt)[match(totalColoc, results_plus_expt)]
+    confInts <- quantile(results_plus_expt, probs = c(0.05, 0.95))
+    
+    # create an output table comparing experimental to shuffled
+    summary <- data.frame(Filename = imageName)
+    summary <- summary %>% mutate(NupCount = nupCount) %>%
+      mutate(ErgCount = ergCount) %>%
+      mutate(NupColoc = totalColoc) %>%
+      mutate(FxnNupColoc = totalColoc/nupCount) %>%
+      mutate(SimColoc = mean(results)) %>%
+      mutate(FxnSimColoc = mean(results)/nupCount) %>% 
+      
+    
+    
+    # visualize results as histogram
+    p <- ggplot(as.data.frame(results), aes(x=results)) + 
+      geom_histogram(binwidth=1) +
+      geom_vline(xintercept = confInts[1], color = "red", alpha = 0.5, linetype = "dashed", linewidth = 2) +
+      geom_vline(xintercept = confInts[2], color = "red", alpha = 0.5, linetype = "dashed", linewidth = 2) +
+      geom_vline(xintercept = totalColoc, color = "blue", alpha = 0.5)
+    
+    # save results
+    
+    # remove any dot from criterion
+    crit <- paste0(c("_within_"),str_replace(as.character(distCriterion), "\\.","x"))
+    
+    # unnest the nn2 results
+    nn2_idx <- nupNearErg[[1]]
+    nn2_dists <- nupNearErg[[2]]
+    nupNearErgDF <- data.frame("NupIndex" = seq(1:nupCount), "NeighborIndex" = nn2_idx, "Distance" = nn2_dists)
+    
+    outputSumm = paste0(imageName,"_summary", crit, ".csv")
+    write_csv(summary, file.path(out, outputSumm))
+    outputNearest = paste0(imageName,"_nearest",crit,".csv")
+    write_csv(nupNearErgDF,file.path(out, outputNearest))
+    outputTrials = paste0(imageName,"_simulations",crit,".csv")
+    write_csv(as.data.frame(results),file.path(out, outputTrials))
+    outputHisto = paste0(imageName,"_simulations_plot",crit,".png")
+    ggsave(file.path(out, outputHisto))
+  } # end NN routine
+  
+  else {
+    print(paste0("Image ",imageName, " contains one or more empty object sets and was not processed."))
   }
-  
-  # rank the actual results vs the shuffled
-  results_plus_expt <- c(results, totalColoc)
-  expt_rank_pct <- percent_rank(results_plus_expt)[match(totalColoc, results_plus_expt)]
-  confInts <- quantile(results_plus_expt, probs = c(0.05, 0.95))
-  
-  # create an output table comparing experimental to shuffled
-  summary <- data.frame(Filename = imageName)
-  summary <- summary %>% mutate(NupCount = nupCount) %>%
-    mutate(ErgCount = ergCount) %>%
-    mutate(NupColoc = totalColoc) %>%
-    mutate(FxnNupColoc = totalColoc/nupCount) %>%
-    mutate(SimColoc = mean(results)) %>%
-    mutate(FxnSimColoc = mean(results)/nupCount)
-  
-  # visualize results as histogram
-  p <- ggplot(as.data.frame(results), aes(x=results)) + 
-    geom_histogram(binwidth=1) +
-    geom_vline(xintercept = confInts[1], color = "red", alpha = 0.5, linetype = "dashed", linewidth = 2) +
-    geom_vline(xintercept = confInts[2], color = "red", alpha = 0.5, linetype = "dashed", linewidth = 2) +
-    geom_vline(xintercept = totalColoc, color = "blue", alpha = 0.5)
-  
-  # save results
-  
-  # remove any dot from criterion
-  crit <- paste0(c("_within_"),str_replace(as.character(distCriterion), "\\.","x"))
-  
-  # unnest the nn2 results
-  nn2_idx <- nupNearErg[[1]]
-  nn2_dists <- nupNearErg[[2]]
-  nupNearErgDF <- data.frame("NupIndex" = seq(1:nupCount), "NeighborIndex" = nn2_idx, "Distance" = nn2_dists)
-  
-  outputSumm = paste0(imageName,"_summary", crit, ".csv")
-  write_csv(summary, file.path(out, outputSumm))
-  outputNearest = paste0(imageName,"_nearest",crit,".csv")
-  write_csv(nupNearErgDF,file.path(out, outputNearest))
-  outputTrials = paste0(imageName,"_simulations",crit,".csv")
-  write_csv(as.data.frame(results),file.path(out, outputTrials))
-  outputHisto = paste0(imageName,"_simulations_plot",crit,".png")
-  ggsave(file.path(out, outputHisto))
   
   
   return()
